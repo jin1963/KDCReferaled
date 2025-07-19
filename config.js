96fba16a3585094ec0a147b9a60372f46a5d00e6
jin1963 <kaojino89@gmail.com>
2025-07-19 21:38:06 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// ✅ Smart Contract Address ของระบบ Referral
+const contractAddress = "0x9B0Abb27524B2857322C2D3682c5bc819eDB6d73";
+
+// ✅ Chain ID ของ BNB Smart Chain Mainnet
+const targetChainId = 56;
+
+// ✅ Token Address ที่ใช้แจก (KJC Token)
+const rewardTokenAddress = "0xd479ae350dc24168e8db863c5413c35fb2044ecd";
